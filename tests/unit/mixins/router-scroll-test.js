6058a8e7c49de1155795209ce16d76344e98a168
Benny C. Wong <bwong337@gmail.com>
2016-07-21 10:55:39 -0700

    es-lint fix

--- tests/unit/mixins/router-scroll-test.js
+++ tests/unit/mixins/router-scroll-test.js
@@ -5,8 +5,8 @@ import { module, test } from 'qunit';
 module('Unit | Mixin | router scroll');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  let RouterScrollObject = Ember.Object.extend(RouterScrollMixin);
-  let subject = RouterScrollObject.create();
+test('it works', (assert) => {
+  const RouterScrollObject = Ember.Object.extend(RouterScrollMixin);
+  const subject = RouterScrollObject.create();
   assert.ok(subject);
 });
