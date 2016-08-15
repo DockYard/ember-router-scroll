5850271db25101e8bb17ad39feafe897cc6a9521
Arjan Singh <arjan.singh@dollarshaveclub.com>
2016-08-15 15:33:51 -0700

    [testing] Use Chrome, not PhantomJS

--- testem.js
+++ testem.js
@@ -9,5 +9,32 @@ module.exports = {
   "launch_in_dev": [
     "PhantomJS",
     "Chrome"
-  ]
+  ],
+  launch_in_ci: [
+    'Chrome Custom',
+  ],
+  launch_in_dev: [
+    'Chrome Custom',
+  ],
+  launchers: {
+    'Chrome Custom': {
+      exe: [
+        'google-chrome',
+        '~/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+        '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+      ],
+      args: [
+        '--user-data-dir=/tmp/testem.chrome.custom',
+        '--no-default-browser-check',
+        '--no-first-run',
+        '--ignore-certificate-errors',
+        '--test-type',
+        '--disable-extensions',
+        '--disable-web-security',
+        '--disable-renderer-backgrounding',
+        '--disable-background-timer-throttling',
+      ],
+      protocol: 'browser',
+    },
+  },
 };
