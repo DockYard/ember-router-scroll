bf9f4a21522ddea6763ca56de034e2ff6ebab619
Jeff Wainwright <yowainwright@gmail.com>
2018-04-02 02:24:50 -0700

    [update] adds linting updates (removes `;` 😁)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,6 @@ module.exports = {
   ],
   env: {
     browser: true
-  }
+  },
+  root: true
 };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-'use strict';
+'use strict'
 
 module.exports = {
-  name: 'ember-router-scroll'
-};
+  name: 'ember-router-scroll',
+}
