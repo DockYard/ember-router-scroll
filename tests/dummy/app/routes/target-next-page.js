03034a60103a51767a804824f84ba03e8b9d4c45
snewcomer <snewcomer24@gmail.com>
2018-04-25 08:43:26 -0700

    modify config in dummy route

--- tests/dummy/app/routes/target-next-page.js
+++ tests/dummy/app/routes/target-next-page.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
+import config from 'dummy/config/environment';
 
 export default Route.extend({
+  init() {
+    this._super(...arguments);
+
+    this.originalConfig = config.routerScroll;
+    config['routerScroll'] = {
+      targetElement: '#main'
+    };
+  },
+
+  willDestroy() {
+    config['routerScroll'] = this.originalConfig;
+  }
 });
