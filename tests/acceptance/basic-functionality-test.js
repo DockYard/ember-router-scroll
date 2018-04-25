e2a269d918130fb5fc8d65fa5be630451b0fc666
snewcomer <snewcomer24@gmail.com>
2018-04-25 11:24:24 -0700

    add scrollY assertion to first test

--- tests/acceptance/basic-functionality-test.js
+++ tests/acceptance/basic-functionality-test.js
@@ -13,18 +13,17 @@ module('Acceptance | basic functionality', function(hooks) {
     await triggerEvent(window, 'scroll');
 
     await click('a[href="/next-page"]');
+    assert.equal(window.scrollY, 0);
 
     assert.equal(currentURL(), '/next-page');
   });
 
   test('The application should work when loading a page and clicking a link to target an element to scroll to', async function(assert) {
     await visit('/target');
-    assert.equal(window.scrollY, 0);
 
     await document.getElementById('monster').scrollIntoView(false);
 
     await click('a[href="/target-next-page"]');
-    assert.equal(window.scrollY, 0);
 
     assert.equal(currentURL(), '/target-next-page');
   });
