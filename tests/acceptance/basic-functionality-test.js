2e131d6cd99b5926ce398d9883f9636ae5366dda
snewcomer <snewcomer24@gmail.com>
2018-04-24 17:05:33 -0700

    add basic acceptance test WIP

--- tests/acceptance/basic-functionality-test.js
+++ tests/acceptance/basic-functionality-test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-import { visit, click, currentURL } from '@ember/test-helpers';
+import { visit, click, currentURL, triggerEvent } from '@ember/test-helpers';
+import config from 'dummy/config/environment';
 
 module('Acceptance | basic functionality', function(hooks) {
   setupApplicationTest(hooks);
@@ -11,5 +12,24 @@ module('Acceptance | basic functionality', function(hooks) {
     await click('a[href="/next-page"]');
 
     assert.equal(currentURL(), '/next-page');
+  });
+
+  test('scott The application should work when loading a page and clicking a link', async function(assert) {
+    let originalConfig = config.routerScroll;
+    config['routerScroll'] = {
+      targetElement: '#main'
+    };
+
+    await visit('/target');
+
+    // TODO: add test.  We have a scrolling container in test land - need to figure this out
+    // await document.getElementById('monster').scrollIntoView(false);
+    // await triggerEvent('#main', 'scroll');
+
+    await click('a[href="/target-next-page"]');
+
+    assert.equal(currentURL(), '/target-next-page');
+
+    config['routerScroll'] = originalConfig;
   });
 });
