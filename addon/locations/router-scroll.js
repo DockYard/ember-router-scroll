719b3eb585451c75af43982dee076f721444a793
snewcomer <snewcomer24@gmail.com>
2019-06-10 20:24:13 -0500

    Deprecate locations/router-scroll for Embers own implementation close #193

--- addon/locations/router-scroll.js
+++ addon/locations/router-scroll.js
@@ -1,5 +1,6 @@
 import HistoryLocation from '@ember/routing/history-location';
 import { set, get } from '@ember/object';
+import { deprecate } from '@ember/application/deprecations';
 
 const uuid = () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
   const r = Math.random() * 16 | 0;
@@ -9,12 +10,30 @@ const uuid = () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) =
 
 export default HistoryLocation.extend({
   pushState(path) {
+    deprecate(
+      `If you are overriding ember-router-scroll's implementation of "pushState", then you can import and override like so: import HistoryLocation from '@ember/routing/history-location';`,
+      false,
+      {
+        id: 'ember-router-scroll',
+        until: '2.0.0'
+      }
+    );
+
     const state = { path, uuid: uuid() };
     get(this, 'history').pushState(state, null, path);
     set(this, '_previousURL', this.getURL());
   },
 
   replaceState(path) {
+    deprecate(
+      `If you are overriding ember-router-scroll's implementation of "replaceState", then you can import and override like so: import HistoryLocation from '@ember/routing/history-location';`,
+      false,
+      {
+        id: 'ember-router-scroll',
+        until: '2.0.0'
+      }
+    );
+
     const state = { path, uuid: uuid() };
     get(this, 'history').replaceState(state, null, path);
     set(this, '_previousURL', this.getURL());
