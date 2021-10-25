f93efe92b3e28ce05f1dead3949ae8af03c10a88
Sergey Astapov <sergeastapov@gmail.com>
2021-10-24 23:54:01 -0400

    Add eslint-plugin-qunit per latest addon blueprint

--- .eslintrc.js
+++ .eslintrc.js
@@ -53,6 +53,11 @@ module.exports = {
       rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
         // add your custom rules and overrides for node files here
       })
-    }
+    },
+    {
+      // Test files:
+      files: ['tests/**/*-test.{js,ts}'],
+      extends: ['plugin:qunit/recommended'],
+    },
   ]
 };
